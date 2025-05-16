6f7f97ff3e2af8ab6fb38299662adc5ce36d7bd2
eddiegulay <edgargulay@outlook.com>
2025-05-16 21:41:40 +0200

    bump version to 0.1.1 in pyproject.toml, setup.cfg, and __init__.py

--- src/modinit/__init__.py
+++ src/modinit/__init__.py
@@ -5,4 +5,4 @@ This package provides a command-line tool to create well-structured project
 directories for AI model training, following best practices in the industry.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
